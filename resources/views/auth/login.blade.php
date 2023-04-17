68a749c9d08ae30617633db5070605a0a3d3d6a6
Julio1205 <raptor.67.jL@gmail.com>
2023-04-16 19:48:27 -0600

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,81 +1,113 @@
 @extends('layouts.app')
 
+  
+
 @section('content')
-<!doctype <!DOCTYPE html>
-<html>
-<head>
 
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<main class="login-form">
 
-    <title>JQuery Login</title>
+  <div class="cotainer">
 
-    <!-- jQuery 3.3.1 -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
+      <div class="row justify-content-center">
 
-    <!-- Bootstrap 4.3.1 -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+          <div class="col-md-8">
 
-    <!-- font awesome library -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+              <div class="card">
 
-    <!-- jQuery Login -->     
-    <script src="assets/js/jquery-login.js"></script>
+                  <div class="card-header">Loginnn</div>
 
-    <!-- main css -->
-    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/dist/style.min.css" />     
+                  <div class="card-body">
 
-</head>
-<body>
-    
-<!-- login area -->
-<aside id="login" class="col-sm-4">    
-    <div class="card">
-        <div class="card-body">
-            <a href="" class="float-right btn btn-outline-primary">Registro</a>
-            <h4 class="card-title mb-4 mt-1">Acceso</h4> <!-- margins -->
-            <p>
-                <!-- social login-->
-                <a href="" class="btn btn-block btn-outline-info"> <i class="fab fa-twitter"></i> Acceso con Twitter</a>
-                <a href="" class="btn btn-block btn-outline-info"> <i class="fab fa-facebook-f"></i> Acceso con Facebook</a>
-            </p>
-            <hr />
-            <!-- start form -->
-            <form name="login_form" id="login_form" action="">
-                <div class="form-group">
-                    <input name="" class="form-control" placeholder="Email or login" type="email" id="email" value="jose@artegrafico.net" />
-                </div>
-                <div class="form-group">
-                    <input class="form-control" placeholder="******" type="password" id="password">
-                </div>                                    
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-block" id="access" name="access">Acceso</button>
-                        </div> 
-                    </div>
-                    <div class="col-md-6 pt-2 pr-4 text-right">
-                        <a class="small" href="#" class="">¿Olvidó su contraseña?</a>
-                    </div>                                            
-                </div>      
-            </form> <!-- end form -->      
-                
-            <!-- messages area -->
-            <div class="row">
-                <div class="col-12">
-                    <div class="outer-loader" id="message-area">
-                        <span class="inner-loader"></span>     
-                    </div>
-                    <span class="debug"></span>    
-                </div>
-            </div> <!-- end messages area-->     
-        </div>
-    </div> 
-</aside> <!-- end login area-->
+  
 
+                      <form action="{{ route('login') }}" method="POST">
 
-</body>
-</html>
+                          @csrf
+
+                          <div class="form-group row">
+
+                              <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+
+                              <div class="col-md-6">
+
+                                  <input type="text" id="email_address" class="form-control" name="email" required autofocus>
+
+                                  @if ($errors->has('email'))
+
+                                      <span class="text-danger">{{ $errors->first('email') }}</span>
+
+                                  @endif
+
+                              </div>
+
+                          </div>
+
+  
+
+                          <div class="form-group row">
+
+                              <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+
+                              <div class="col-md-6">
+
+                                  <input type="password" id="password" class="form-control" name="password" required>
+
+                                  @if ($errors->has('password'))
+
+                                      <span class="text-danger">{{ $errors->first('password') }}</span>
+
+                                  @endif
+
+                              </div>
+
+                          </div>
+
+  
+
+                          <div class="form-group row">
+
+                              <div class="col-md-6 offset-md-4">
+
+                                  <div class="checkbox">
+
+                                      <label>
+
+                                          <input type="checkbox" name="remember"> Remember Me
+
+                                      </label>
+
+                                  </div>
+
+                              </div>
+
+                          </div>
+
+  
+
+                          <div class="col-md-6 offset-md-4">
+
+                              <button type="submit" class="btn btn-primary">
+
+                                  Login
+
+                              </button>
+
+                          </div>
+
+                      </form>
+
+                        
+
+                  </div>
+
+              </div>
+
+          </div>
+
+      </div>
+
+  </div>
+
+</main>
+
 @endsection
