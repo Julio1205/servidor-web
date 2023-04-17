8bca278652cc7354baaa2fb649a7f82ee4a1982e
Julio1205 <raptor.67.jL@gmail>
2023-04-17 01:38:12 +0000

    i

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/login',[App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
+//Route::get('/login',[App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
 
 
